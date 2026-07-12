557b69a1da72e63af073dfad43bc1914862ea539
ejulien-42 <ejulien@student.42nice.fr>
2026-07-13 01:44:44 +0200

    cleaned

--- src_other/__init__.py
+++
@@ -1,9 +0,0 @@
-from .Input import Type, Parameter, Function, Prompt
-from .InputParser import InputParser
-from .PipelineManager import PipelineManager
-from .GenerationPipeline import GenerationPipeline
-from .ConstrainingDecoder import ConstrainingDecoder
-
-__all__ = ["Type", "Parameter", "Function", "Prompt",
-           "InputParser", "PipelineManager",
-           "GenerationPipeline", "ConstrainingDecoder"]
